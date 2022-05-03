e3a47044adf3df29ba57752ed9289eedba417fb9
Damon Cook <damonacook@gmail.com>
2022-05-03 17:57:25 -0400

    Add Style Guide: Colors pattern

---
+++ patterns/style-guide-section__color-palette.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Title:       Style Guide: color palette
+ * Slug:        extendable/style-guide-section__color-palette
+ * Categories:  extendable-style-guide
+ */
+?>
+
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div id="style-guide-section--color-palette" class="wp-block-group"><!-- wp:heading -->
+<h2 id="style-guide__color-palette">Color palette</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center" id="style-guide__colors"><!-- wp:column {"verticalAlignment":"center","width":"20%","style":{"spacing":{"padding":{"top":"3rem","right":"1rem","bottom":"3rem","left":"1rem"}}},"backgroundColor":"foreground","textColor":"background"} -->
+<div class="wp-block-column is-vertically-aligned-center has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:3rem;padding-right:1rem;padding-bottom:3rem;padding-left:1rem;flex-basis:20%"><!-- wp:paragraph -->
+<p>Foreground:<br>#000000</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"20%","style":{"spacing":{"padding":{"top":"3rem","right":"1rem","bottom":"3rem","left":"1rem"}}},"backgroundColor":"background","textColor":"foreground"} -->
+<div class="wp-block-column is-vertically-aligned-center has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:3rem;padding-right:1rem;padding-bottom:3rem;padding-left:1rem;flex-basis:20%"><!-- wp:paragraph -->
+<p>Background:<br>#ffffff</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"20%","style":{"spacing":{"padding":{"top":"3rem","right":"1rem","bottom":"3rem","left":"1rem"}}},"backgroundColor":"primary","textColor":"background"} -->
+<div class="wp-block-column is-vertically-aligned-center has-background-color has-primary-background-color has-text-color has-background" style="padding-top:3rem;padding-right:1rem;padding-bottom:3rem;padding-left:1rem;flex-basis:20%"><!-- wp:paragraph -->
+<p>Primary:<br>#005450</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"20%","style":{"spacing":{"padding":{"top":"3rem","right":"1rem","bottom":"3rem","left":"1rem"}}},"backgroundColor":"secondary","textColor":"background"} -->
+<div class="wp-block-column is-vertically-aligned-center has-background-color has-secondary-background-color has-text-color has-background" style="padding-top:3rem;padding-right:1rem;padding-bottom:3rem;padding-left:1rem;flex-basis:20%"><!-- wp:paragraph -->
+<p>Secondary:<br>#551a8b</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"20%","style":{"spacing":{"padding":{"top":"3rem","right":"1rem","bottom":"3rem","left":"1rem"}}},"backgroundColor":"tertiary","textColor":"background"} -->
+<div class="wp-block-column is-vertically-aligned-center has-background-color has-tertiary-background-color has-text-color has-background" style="padding-top:3rem;padding-right:1rem;padding-bottom:3rem;padding-left:1rem;flex-basis:20%"><!-- wp:paragraph -->
+<p>Tertiary:<br>#004d49</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->

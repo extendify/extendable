f6325047ec39d472e84ffb1b5a656ac0a03cae96
Damon Cook <damonacook@gmail.com>
2022-05-03 17:57:44 -0400

    Add Style Guide: Typography pattern

---
+++ patterns/style-guide-section__typography.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Title:       Style Guide: typography
+ * Slug:        extendable/style-guide-section__typography
+ * Categories:  extendable-style-guide
+ */
+?>
+
+<!-- wp:group {"lock":{"move":true,"remove":true}} -->
+<div id="style-guide-section--typography" class="wp-block-group"><!-- wp:heading -->
+<h2 id="style-guide__typography">Typography</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group -->
+<div id="style-guide__lists" class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list -->
+<ul><li>Magna cupidatat occaecat ut ex sunt excepteur commodo duis adipiscing ea culpa</li><li>Reprehenderit ad irure reprehenderit<ul><li>Sunt velit aute sit labore</li><li>Ad adipiscing excepteur</li></ul></li><li>Deserunt enim officia ex deserunt do pariatur</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"ordered":true} -->
+<ol><li>Consequat lorem aliqua excepteur sint anim<ol><li>Pariatur aliquip adipiscing sed eu tempor</li><li>Commodo incididunt commodo sed</li></ol></li><li>Laboris dolor amet dolore esse sit consectetur veniam non ea aliqua nulla.</li><li>Amet pariatur sit consequat</li><li>Do non commodo amet.</li></ol>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"ordered":true} -->
+<ol><li>Ut pariatur aute sit magna sit.</li><li>Do quis veniam ex aute commodo duis ea officia non<ul><li>Irure nisi laboris nulla excepteur.</li><li>Quis aliquip nisi amet.<ol><li>Velit esse magna mollit ad nulla</li><li>Aliqua culpa minim</li></ol></li><li>Consequat voluptate qui magna est</li></ul></li><li>Mollit do reprehenderit eiusmod incididunt esse sunt non ad irure quis ipsum</li><li>Reprehenderit sed mollit cillum aliqua dolore amet consequat fugiat sunt.</li></ol>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list -->
+<ul><li>Cupidatat sit pariatur laborum est in cillum eu</li><li>Occaecat non sit id</li><li>Nostrud consequat qui laborum sunt consectetur minim<ol><li>Aliqua tempor aliquip dolore sunt reprehenderit dolore mollit</li><li>Reprehenderit quis eu adipiscing<ul><li>Magna lorem mollit do</li></ul></li><li>Eu nulla quis id</li></ol></li><li>Aliqua ad aliquip labore do proident excepteur in.</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div id="style-guide__headings" class="wp-block-group"><!-- wp:heading {"level":1} -->
+<h1 id="heading-one">Heading One</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading -->
+<h2 id="heading-two">Heading Two</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3} -->
+<h3 id="heading-three">Heading Three</h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":4} -->
+<h4 id="heading-four">Heading Four</h4>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":5} -->
+<h5 id="heading-five">Heading Five</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":6} -->
+<h6 id="heading-six">Heading Six</h6>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

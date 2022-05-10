253c5aed15c883f14d26f690c65c2344d702f3b4
Damon Cook <damonacook@gmail.com>
2022-05-10 18:28:49 -0400

    Add patterns/header-default.php

---
+++ patterns/header-default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title:       Header (Logo, Navigation)
+ * Slug:        extendable/header-default
+ * Categories:  extendable-headers
+ * blockTypes:  core/template-part/header
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"inherit":true},"style":{"spacing":{"padding":{"bottom":"var(--wp--style--block-gap)","top":"var(--wp--style--block-gap)"}}}} -->
+<div class="wp-block-group alignfull" style="padding-bottom:var(--wp--style--block-gap);padding-top:var(--wp--style--block-gap)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":120} /-->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:site-title /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+
+<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"right"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

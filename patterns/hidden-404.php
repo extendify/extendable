38e67e70d9f2a6f2c0481e204998153102364769
“Amjad <amjadr360@gmail.com>
2023-11-28 16:21:32 +0500

    update search page string

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -9,15 +9,14 @@
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60)">
 
-<!-- wp:heading {"level":2} -->
-<h2><?php echo esc_html__( 'Not found, error 404', 'extendable' ); ?></h2>
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading" id="page-not-found"><?php echo esc_html_x( 'Page Not Found', 'Heading for a webpage that is not found', 'extendable' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html__( 'Oops, the page you are looking for does not exist or is no longer available. We are sorry about that. Just use the search form to find your way.', 'extendable' ); ?></p>
+<p><?php echo esc_html_x( 'The page you are looking for does not exist, or it has been moved. Please try searching using the form below.', 'Message to convey that a webpage could not be found', 'extendable' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:search {"width":75,"widthUnit":"%","showLabel":false,"buttonText":"Search"} /-->
-
+<!-- wp:pattern {"slug":"extendable/hidden-search"} /-->
 </div>
 <!-- /wp:group -->

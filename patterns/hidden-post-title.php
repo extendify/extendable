e446ed60791f79f4cdbff0f96056b4c49f2be87f
“Amjad <amjadr360@gmail.com>
2025-06-05 01:04:04 +0500

    Update template

---
+++ patterns/hidden-post-title.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Post Title
+ * Slug: extendable/hidden-post-title
+ * Inserter: no
+ *
+ * @package Extendable
+ * @since Extendable 2.0.24
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Page title"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"tertiary","layout":{"inherit":true,"type":"constrained","justifyContent":"center"}} -->
+	<div class="wp-block-group alignfull has-tertiary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+        <!-- wp:post-title {"level":1,"align":"wide","fontSize":"large"} /-->
+    </div>
+<!-- /wp:group -->

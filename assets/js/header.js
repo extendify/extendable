338212ba60ee31feaa571530a8e881da39c417cf
“Amjad <amjadr360@gmail.com>
2026-04-30 23:08:42 +0500

    add new 4 navbars and tweak some code

--- assets/js/header.js
+++ assets/js/header.js
@@ -8,4 +8,19 @@
 		const height = entries[ 0 ].borderBoxSize[ 0 ].blockSize;
 		document.documentElement.style.setProperty( '--ext-header-height', height + 'px' );
 	} ).observe( header, { box: 'border-box' } );
+
+	const hero = document.querySelector( '.entry-content > .ext-hero-section.ext-hero-section--full-screen:first-child' );
+	if ( hero ) {
+		new IntersectionObserver( function ( entries ) {
+			header.classList.toggle( 'is-past-hero', ! entries[ 0 ].isIntersecting );
+		}, { rootMargin: '-50% 0px 0px 0px' } ).observe( hero );
+	}
+
+	const topSentinel = document.createElement( 'div' );
+	topSentinel.style.cssText = 'position:absolute;top:0;left:0;height:1px;width:1px;pointer-events:none;';
+	document.body.prepend( topSentinel );
+
+	new IntersectionObserver( function ( entries ) {
+		header.classList.toggle( 'is-scrolled', ! entries[ 0 ].isIntersecting );
+	} ).observe( topSentinel );
 } )();
